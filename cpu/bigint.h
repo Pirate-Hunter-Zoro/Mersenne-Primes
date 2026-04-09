4d23bed0a15d84b56bde770a5e5e257ab922f158
Mikey Ferguson <mtf6056@gmail.com>
2026-04-09 13:26:31 -0500

    addition and modular arithmetic implemented

--- cpu/bigint.h
+++ cpu/bigint.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <cstdint>
+#include <algorithm>
 
 struct BigInt {
     std::vector<uint32_t> limbs;
@@ -107,26 +108,118 @@ struct BigInt {
         return result;
     }
 
+    static BigInt add(const BigInt& a, const BigInt& b) {
+        // BigInt result to return
+        BigInt result = BigInt(0);
+        // If a has n limbs, and b has n limbs, then the result has at most n+m limbs
+        int i = a.limbs.size();
+        int j = b.limbs.size();
+
+        // Before moving on to the algorithm, resize the results vector to have length matching 'a'
+        for (int k = 1; k < std::max(i, j)+1; k++)
+        {
+            result.limbs.push_back(0);
+        }
+
+        // Perform addition
+        uint32_t carry = 0;
+        uint32_t a_val;
+        uint32_t b_val;
+        uint64_t sum;
+        uint32_t lower_sum;
+        for (int k=0; k<std::max(i,j); k++) {
+            if (k < i) {
+                a_val = a.limbs.at(k);
+            } else {
+                a_val = 0;
+            }
+            if (k < j) {
+                b_val = b.limbs.at(k);
+            } else {
+                b_val = 0;
+            }
+            // Compute sum and carry
+            sum = (uint64_t)a_val + b_val + carry;
+            lower_sum = sum & 0xFFFFFFFF;
+            result.limbs[k] = lower_sum;
+            carry = sum >> 32;
+        }
+        if (carry > 0) {
+            result.limbs[std::max(i,j)] = carry;
+        }
+
+        // Remove trailing zeros
+        int idx = result.limbs.size() - 1;
+        while (result.limbs.size() > 1 && result.limbs.at(idx) == 0)
+        {
+            result.limbs.erase(result.limbs.begin() + idx);
+            idx--;
+        }
+        return result;
+    }
+
     // Modulo - we don't actually need the mersenne number; only the power which it results from
     static BigInt mod_mersenne(const BigInt& a, int p) {
-        BigInt result = BigInt(0);
-        // Split x at bit position p
+        BigInt result = a;
+
+        // Split at bit position p
         int bin_num = p / 32;
         int split_point = p % 32;
-        uint32_t x = a.limbs.at(bin_num);
-        for (int i=0; i<bin_num; i++) {
-            if (i==0) {
-                result.limbs[i] = a.limbs.at(i);
-            } else {
-                result.limbs.push_back(a.limbs.at(i));
+        bool equal;
+
+        while ((result.limbs.size() > bin_num+1) || (result.limbs.size() == bin_num+1 && (result.limbs.at(result.limbs.size()-1) >= (1u << split_point)))) { 
+            // While result does not fit within p bits
+            BigInt low_result = BigInt(0);
+            uint32_t x = result.limbs.at(bin_num);
+            for (int i=0; i<bin_num; i++) {
+                if (i==0) {
+                    low_result.limbs[i] = result.limbs.at(i);
+                } else {
+                    low_result.limbs.push_back(result.limbs.at(i));
+                }
             }
+            // Now add the split point
+            uint32_t lower_bits_split = x & ((1u << split_point)-1u);
+            if (bin_num > 0) {
+                low_result.limbs.push_back(lower_bits_split);
+            } else {
+                low_result.limbs[0] = lower_bits_split;
+            }
+
+            // Now we need the higher bits in the split 
+            uint32_t higher_bits_split = x >> split_point;
+            BigInt high_result = BigInt(higher_bits_split);
+            for (int i=bin_num+1; i<result.limbs.size(); i++) {
+                // Shift in the lower 32-split_point bits from this bin into the previous new bin
+                high_result.limbs[high_result.limbs.size()-1] += result.limbs.at(i) << (32 - split_point);
+                // Shift in the upper split_point bits from this bin into the first split_point bits of the current new bin
+                high_result.limbs.push_back(result.limbs.at(i) >> split_point);
+            }
+
+            result = add(low_result, high_result);
         }
-        // Now add the split point
-        uint32_t lower_bits_split = x & ((1u << split_point)-1u);
-        if (bin_num > 0) {
-            result.limbs.push_back(lower_bits_split);
-        } else {
-            result.limbs[0] = lower_bits_split;
+
+        // Edge case - check for equality in which case return 0
+        if (result.limbs.size() == bin_num + 1)
+        {
+            equal = 1;
+            for (int i = 0; i <= bin_num; i++)
+            {
+                if ((i < bin_num) && (result.limbs.at(i) != 0xFFFFFFFF))
+                {
+                    equal = 0;
+                    break;
+                }
+                else if ((i == bin_num) && (result.limbs.at(i) != (1u << split_point) - 1))
+                {
+                    equal = 0;
+                    break;
+                }
+            }
+            if (equal)
+            {
+                return BigInt(0);
+            }
         }
 
         return result;
